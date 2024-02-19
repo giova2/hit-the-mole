976595c02b560bb498fa735ea339f7c29929ff11
franco giovannangelo <fcgiova@gmail.com>
2024-02-19 18:32:44 -0300

    first version

--- app.js
+++ app.js
@@ -1,49 +1,106 @@
-const square = document.querySelectorAll('.square')
+const squares = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
-let score = document.querySelector('#score')
+const score = document.querySelector('#score')
+const playAgainButton = document.body.querySelector('#playAgain')
+const stopGameButton = document.body.querySelector('#stopGame')
+const difficulty = document.querySelector('#difficulty')
 
 let result = 0
 let currentTime = timeLeft.textContent
+let hitPosition;
+let countDownTimerId
+let moveMoleTimerId
+let velocity = 500 // medium
 
 function randomSquare() {
-  square.forEach(className => {
+  squares.forEach(className => {
     className.classList.remove('mole')
   })
-  let randomPosition = square[Math.floor(Math.random() * 9)]
+  let randomPosition = squares[Math.floor(Math.random() * 9)]
   randomPosition.classList.add('mole')
 
   //assign the id of the randomPosition to hitPosition for us to use later
   hitPosition = randomPosition.id
 }
 
-square.forEach(id => {
-  id.addEventListener('mouseup', () => {
-    if(id.id === hitPosition){
+const moveMole = () => {
+  return setInterval(randomSquare, velocity)
+}
+
+squares.forEach(square => {
+  square.addEventListener('mouseup', () => {
+    if(square.id === hitPosition){
+      square.classList.add('catched')
+      setTimeout(() => {
+        square.classList.remove('catched')
+      }, velocity - 100);
       result = result + 1
       score.textContent = result
-      hitPosition=null
+      hitPosition = null
     }
   })
 })
 
 
-function moveMole() {
-  let timerId = null
-  timerId = setInterval(randomSquare, 500)
+const stopGame = () => {
+  timeLeft.textContent = 0
+  clearInterval(countDownTimerId)
+  clearInterval(moveMoleTimerId)
+  stopGameButton.classList.add('hidden')
+  playAgainButton.classList.remove('hidden')
+  difficulty.classList.remove('hidden')
 }
 
-moveMole()
-
-
-function countDown() {
+const countDown = () => {
   currentTime--
   timeLeft.textContent = currentTime
 
   if(currentTime === 0 ) {
-    clearInterval(timerId)
-    alert('GAME OVER! Your final score is' + result)
+    stopGame()
+    alert(`Round finished! Your final score is ${result}`)
   }
 }
 
-let timerId = setInterval(countDown, 1000)
+const startCountDown = () => (setInterval(countDown, 1000))
+
+const setDifficulty = () => {
+  const radios = document.getElementsByName('difficulty');
+
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      velocity = parseInt(radios[i].value);
+      break;
+    }
+  }
+}
+
+// moveMoleTimerId = moveMole()
+// countDownTimerId = startCountDown()
+
+
+
+const restartGame = () => {
+  setDifficulty();
+  // restart timer
+  currentTime = 60;
+  timeLeft.textContent = currentTime;
+  // restart results
+  result = 0
+  score.textContent = result
+  // restart game
+  moveMoleTimerId = moveMole()
+  countDownTimerId = startCountDown()
+}
+
+stopGameButton.addEventListener('click', () => {
+  stopGame();
+})
+
+playAgainButton.addEventListener('click', () => {
+  stopGameButton.classList.remove('hidden');
+  playAgainButton.classList.add('hidden');
+  playAgainButton.textContent = 'Play Again!';
+  difficulty.classList.add('hidden');
+  restartGame();
+})
